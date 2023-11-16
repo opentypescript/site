8cc41085d80dac62bf044e2b3bc1841c4b0a64b1
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-11-16 05:53:40 +0000

    ssr implemented

--- components/globals/toolbar.tsx
+++ components/globals/toolbar.tsx
@@ -1,38 +1,32 @@
-'use client'
+// toolbar.tsx
+import { siteConfig } from "@/config/site";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa6";
+import { RelativeDate } from "./relativedate";
+import { GetStaticProps } from 'next';
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-import Link from "next/link"
-import { FaGithub } from "react-icons/fa6"
-import { RelativeDate } from "./relativedate"
-
-const dateModified = new Date();
-
-export async function getStaticProps() {
-    return {
-        props: {
-            RelativeDate,
-        },
-       revalidate: 60, // In seconds
-    }
+interface ToolBarProps {
+  RelativeDate: React.ComponentType<{ prefix: string; date: Date | null }>;
 }
 
-export default function ToolBar() {
-    return (
+const ToolBar: React.FC<ToolBarProps> = ({ RelativeDate }) => {
+  // Use the current date as the modification date for demonstration purposes
+  const dateModified = new Date();
+
+  return (
     <section>
-    <div className="flex items-center space-x-4 sm:justify-between sm:space-x-0">
-        <span className="text-blue-700 font-bold text-md"> <RelativeDate date={dateModified} prefix="Last updated" /> </span>
-     <div className="flex flex-1 items-center justify-end space-x-4">
+      <div className="flex items-center space-x-4 sm:justify-between sm:space-x-0">
+        <span className="text-blue-700 font-bold text-md">
+          <RelativeDate date={dateModified} prefix="Last updated" />
+        </span>
+        <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center ">
-            <Link
-              href={siteConfig.links.contribute}
-              target="_blank"
-              rel="noreferrer"
-            >
+            <Link href={siteConfig.links.contribute} target="_blank" rel="noreferrer">
               <div className={buttonVariants()}>
                 <div className="inline-flex items-center py-1 px-2 space-x-3 rounded-md  ">
                   <FaGithub className="h-6 w-6" />
-                  <text className="font-semibold">Contribute</text>
+                  <span className="font-semibold">Contribute</span>
                 </div>
               </div>
             </Link>
@@ -40,7 +34,16 @@ export default function ToolBar() {
         </div>
       </div>
     </section>
-    )
-}
+  );
+};
 
+export const getStaticProps: GetStaticProps<ToolBarProps> = async () => {
+  return {
+    props: {
+      RelativeDate,
+    },
+    revalidate: 60, // In seconds
+  };
+};
 
+export default ToolBar;
